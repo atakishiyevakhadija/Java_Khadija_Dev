17b0937722a1a2810d612a743eec21c00433aa30
atakishiyevakhadija <atakishiyevakhadija@gmail.com>
2025-09-20 18:57:16 +0400

    Homework Khadija

---
+++ Java_Khadija_Dev/src/Homework/RandomNumbers.java
@@ -0,0 +1,8 @@
+package Homework;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {20, 118, 150, 170, 178, 181, 187, 189, 190, 198};
+        System.out.println(numbers[1] + " " + numbers[2] + " " + numbers[3] + " " + numbers[4] + " " + numbers[5] + " " + numbers[6] + " " + numbers[7] + " " + numbers[8] + " " + numbers[9]);
+    }
+}
